aaa76ebbec04d40e58c7039c5a7998f704c05c93
SayandeepBera <sayandeep4002@gmail.com>
2025-04-17 03:20:54 +0530

    Add infinite Scroll Bar instade of previous and next button

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import NewsItems from "./NewsItems";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
-// import InfiniteScroll from "react-infinite-scroll-component";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -33,43 +33,54 @@ export default class News extends Component {
   }
 
   async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=27412fcfbe7f489984bcb6560346821c&page=${this.state.page}&pagesize=${this.props.pageSize}`;
+    this.props.setProgress(10);
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pagesize=${this.props.pageSize}`;
     this.setState({ loading: true });
-    let data = await fetch(url);
-    let preseData = await data.json();
-    this.setState({
-      articles: preseData.articles,
-      totalArticles: preseData.totalResults,
-      loading: false,
-    });
+
+    try {
+      let data = await fetch(url);
+      let preseData = await data.json();
+
+      if (preseData.articles) {
+        this.setState({
+          articles: preseData.articles,
+          totalArticles: preseData.totalResults || 0,
+          loading: false,
+        });
+      } else {
+        console.error("API response invalid", preseData);
+        this.setState({
+          articles: [],
+          totalArticles: 0,
+          loading: false,
+        });
+      }
+      this.props.setProgress(100);
+    } catch (error) {
+      console.error("Error fetching news", error);
+      this.setState({ articles: [], loading: false });
+    }
   }
+  
 
   async componentDidMount() {
     this.updateNews();
   }
 
-  handlePrevClick = async () => {
-    this.setState(
-      (prevState) => ({ page: prevState.page - 1 }),
-      () => this.updateNews()
-    );
-  };
-
-  handleNextClick = async () => {
-    this.setState(
-      (prevState) => ({ page: prevState.page + 1 }),
-      () => this.updateNews()
-    );
-    
-  };
-
   fetchMoreData = async () => {
     const nextPage = this.state.page + 1;
   
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=27412fcfbe7f489984bcb6560346821c&page=${nextPage}&pagesize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${nextPage}&pagesize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let preseData = await data.json();
+
+    console.log(preseData);
+    console.log(this.state.articles.length);
+    console.log(this.state.totalArticles);
+    console.log(this.state.articles);
+    console.log("page = ",this.state.page);
 
     this.setState({
       articles: this.state.articles.concat(preseData.articles),
@@ -82,15 +93,20 @@ export default class News extends Component {
 
   render() {
     return (
-      <div className="container">
+      <>
         <h1 className="text-center" style={{ margin: "30px 0" }}>
           NewsMonkey - Top {this.cpaitalizeTitle(this.props.category)} Headlines
         </h1>
-        {this.state.loading && <Spinner/>}
+    
+        <InfiniteScroll
+          dataLength={this.state.articles ? this.state.articles.length : 0}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length < (this.state.totalArticles)}
+          loader={<Spinner/>}>
 
-        <div>
+          <div className="container">
             <div className="row">
-              {!this.state.loading && this.state.articles.map((element) => {
+              {this.state.articles.map((element) => {
                   return (
                     <div className="col-md-4" key={element.url}>
                       <NewsItems title={element.title ? element.title.slice(0, 48) : ""} description={element.description ? element.description.slice(0, 90) : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://www.washingtonpost.com/wp-apps/imrs.php?src=https://arc-anglerfish-washpost-prod-washpost.s3.amazonaws.com/public/L7MPOTG64US6RSYEEM6M2NLVMQ_size-normalized.jpg&w=1440"} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
@@ -99,12 +115,9 @@ export default class News extends Component {
                 })}
             </div>
           </div>
+        </InfiniteScroll>
 
-        <div className="container d-flex justify-content-between my-4">
-          <button type="button" disabled={this.state.page <= 1} className="btn btn-warning" onClick={this.handlePrevClick}> &lArr; Previous </button>
-          <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalArticles / this.props.pageSize)} className="btn btn-warning" onClick={this.handleNextClick}> Next &rArr; </button>
-        </div>
-      </div>
+      </>
     );
   }
 }
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -7,9 +7,12 @@ export default class NewsItems extends Component {
       <div>
         <div className="my-3">
         <div className="card" style={{width: "24rem"}}>
-            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left : "93%", zIndex : 1}}>
-              {source}
-            </span>
+            <div style={{display : "flex", position : "absolute", right : "0"}}>
+              <span className="badge rounded-pill bg-danger">
+                {source}
+              </span>
+            </div>
+
             <img src={imageUrl} alt="/"/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
